34d22f6fdae4087c7e315209315f30a124f334a9
Miki <rootssl@mail.ru>
2021-05-12 21:31:19 +0300

    task 2 finished

--- js/questions.js
+++ js/questions.js
@@ -1,11 +1,8 @@
 "use strict";
-//Создать переменную  numberOfFilms и поместить туда ответ от пользователя на вопрос "сколько фильмов вы уже посмотрели?"
+//Task 1
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-
-// document.write(numberOfFilms);
-
-let userAnswerBlock = document.createElement('div'),
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"),
+    userAnswerBlock = document.createElement('div'),
     heading = document.createElement('h2');
 
 userAnswerBlock.classList.add('block'); //create class block
@@ -13,3 +10,13 @@ heading.classList.add('message');       //create class message
 heading.textContent = numberOfFilms;
 document.body.appendChild(userAnswerBlock);
 userAnswerBlock.appendChild(heading);
+
+//Task 2
+let arr = []; //create empty array
+const personaMovieDb = {
+    count: numberOfFilms,
+    movies: "",
+    actors: "",
+    genres: arr,
+    privat: false
+};
