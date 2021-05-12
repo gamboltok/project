97a485ab75469617cad64519aa0bd121748ea662
Miki <rootssl@mail.ru>
2021-05-12 20:55:20 +0300

    lesson 1 completed

---
+++ js/questions.js
@@ -0,0 +1,6 @@
+"use strict";
+//Создать переменную  numberOfFilms и поместить туда ответ от пользователя на вопрос "сколько фильмов вы уже посмотрели?"
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+document.write(numberOfFilms);
