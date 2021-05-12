8591da243a910ec54b93af094c07f513560e8f4e
Miki <rootssl@mail.ru>
2021-05-12 21:54:55 +0300

    Task 3

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,7 @@
 "use strict";
 //Task 1
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"),
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ""),
     userAnswerBlock = document.createElement('div'),
     heading = document.createElement('h2');
 
@@ -11,12 +11,21 @@ heading.textContent = numberOfFilms;
 document.body.appendChild(userAnswerBlock);
 userAnswerBlock.appendChild(heading);
 
-//Task 2
-let arr = []; //create empty array
+//Task 2 & 3 combined
 const personaMovieDb = {
     count: numberOfFilms,
-    movies: "",
-    actors: "",
-    genres: arr,
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 };
+
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingFilm = prompt('На сколько оцените его?', ''),
+      lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
+      ratingFilm2 = prompt('На сколько оцените его?', '');
+
+      personaMovieDb.movies[lastFilm1] = ratingFilm;
+      personaMovieDb.movies[lastFilm2] = ratingFilm2;
+
+console.log(personaMovieDb);
