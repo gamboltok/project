2cd61346fc2758970935ba5769b95a0d20d01de4
Miki <rootssl@mail.ru>
2021-05-16 21:02:58 +0300

    fixed

--- js/questions.js
+++ js/questions.js
@@ -1,27 +1,27 @@
 "use strict";
 
-let numberOfFilms;
+// let numberOfFilms;
 
 const personaMovieDb = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: true,
       start: function(){
-        numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+        personaMovieDb.count = prompt("Сколько фильмов вы уже посмотрели?", "");
         let userAnswerBlock = document.createElement('div'),
             heading = document.createElement('h2');
       
               userAnswerBlock.classList.add('block'); //create class block
               heading.classList.add('message');       //create class message
       
-                heading.textContent = numberOfFilms;
+                heading.textContent = personaMovieDb.count;
                 document.body.appendChild(userAnswerBlock);
                 userAnswerBlock.appendChild(heading);
       
-                  while(numberOfFilms == "" ||numberOfFilms == null || isNaN(numberOfFilms)){
-                    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+                  while(personaMovieDb.count == "" || personaMovieDb.count == null || isNaN(personaMovieDb.count)){
+                    personaMovieDb.count = prompt("Сколько фильмов вы уже посмотрели?", "");
                   }
       },
       rememberFilm: function(){
@@ -44,11 +44,27 @@ const personaMovieDb = {
           console.log("personaMovieDb приватная");
         }
       },
-      whiteYourGenres: function(){
+      writeYourGenres: function(){
         let arr = [];
         for(let i = 0; i < 3; i++){
           let question = prompt(`Ваш любимый жанр №${i+1}?`, "");
-          personaMovieDb.genres[i] = question;
+            if(question == "" || question == null){
+              console.log("заполните правильно");
+              i--;
+              let question = prompt(`Ваш любимый жанр №${i+1}?`, "");
+            }else{
+              personaMovieDb.genres[i] = question;
+            }
+        }
+        personaMovieDb.genres.forEach((value, i) => {
+          console.log(`Жанр №${i+1}: ${value}`);
+        });
+      },
+      toggleVisibleMyDb: function(){
+        if(personaMovieDb.privat == false){
+          personaMovieDb.privat = true;
+        }else{
+          personaMovieDb.privat = false;
         }
       }
 
@@ -56,5 +72,6 @@ const personaMovieDb = {
 
 personaMovieDb.start();
 personaMovieDb.rememberFilm();
+personaMovieDb.toggleVisibleMyDb();
 personaMovieDb.showMyDb(personaMovieDb.privat);
-personaMovieDb.whiteYourGenres();
+personaMovieDb.writeYourGenres();
